b2432b0740ee1e549ba2fa948828dedb37b99495
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-12-01 10:42:26 +0200

    added knex, test GET endpoints

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,13 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('Spiegel', 'https://www.spiegel.de/',
+    'Nachrichten', '3'),
+  ('i24', 'https://www.i24news.tv/en',
+    'Mein Arbeitgeber', '1'),
+  ('Slack', 'https://app.slack.com/',
+    '', '4'),
+  ('NY Times', 'https://www.nytimes.com/',
+    'Noch mehr Nachrichten', '5'),
+  ('Chefkoch', 'https://www.chefkoch.de/',
+    'Rezepte', '2');
+  
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,44 @@
+function makeBookmarksArray() {
+    return [
+        {
+        id: 1,
+        title: 'Test-Bookmark 1',
+        url: 'www.test-bookmark-1.com',
+        description: 'Description test bookmark 1',
+        rating: 5   
+           },
+           {
+        id: 2,
+        title: 'Test-Bookmark 2',
+        url: 'www.test-bookmark-2.com',
+        description: 'Description test bookmark 2',
+        rating: 4   
+            },
+            {
+        id: 3,
+        title: 'Test-Bookmark 3',
+        url: 'www.test-bookmark-3.com',
+        description: 'Description test bookmark 3',
+        rating: 3   
+            },
+            {
+        id: 4,
+        title: 'Test-Bookmark 4',
+        url: 'www.test-bookmark-4.com',
+        description: 'Description test bookmark 4',
+        rating: 2   
+            },
+            {
+        id: 5,
+        title: 'Test-Bookmark 5',
+        url: 'www.test-bookmark-5.com',
+        description: 'Description test bookmark 5',
+        rating: 1   
+            },
+         ];
+}
+
+module.exports = {
+    makeBookmarksArray,
+  }
+    
